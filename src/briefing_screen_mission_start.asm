95d91ace0b420298e0372e6d2d8c992d251ef44d
Iran <ma1kelvdh@gmail.com>
2014-04-27 22:42:16 +0200

    fix black color to be green like it should on buttons in briefing screen after scenario load

--- src/briefing_screen_mission_start.asm
+++ src/briefing_screen_mission_start.asm
@@ -8,21 +8,16 @@ _Start_Scenario_Force_Briefing_Screen:
     
     call    0x0059CC40 ; loads bunch of resources
     
-    mov dword [0x00808B7C], 0x23D05E
     
-    mov     dword [0x809218], 1
-    mov     dword [0x809250], 28h
-    mov     dword [0x808B6C], 7Fh
-    mov     dword [0x808B68], 109010h
-    mov     dword [0x809248], 0x909090
-
-    mov     dword [0x809244], 0DCB64Eh
-    mov     dword [0x809230], 615022h
-    mov     dword [0x8093A4], 221B0Bh
-    mov     dword [0x808E30], 443716h
-
+    mov     ecx, 0
+    push    ecx
+    call    [0x006CA368]
+    push    eax
+    call    0x005912E0
+    
     mov     ecx, [ScenarioStuff]
     call    0x005C0230
+
     
     pop     ecx
     
