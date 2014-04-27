9e1b1d789ab35a0655d291300bcb710b1771efa5
Iran <ma1kelvdh@gmail.com>
2014-04-27 18:44:03 +0200

    commit code to remove blowfish dependency

---
+++ src/no_blowfish_dll.asm
@@ -0,0 +1,7 @@
+@CLEAR 0x005FFE8A 0x90 0x005FFEA5
+@CLEAR 0x005FFEC2 0x90 0x005FFEC9
+
+@JMP 0x00600F76 _Continue_When_BlowFish_COM_Object_Cant_Be_Loaded
+
+_Continue_When_BlowFish_COM_Object_Cant_Be_Loaded:
+    jmp     0x00600FA3
