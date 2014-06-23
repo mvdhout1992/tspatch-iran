1d5b5502efe80ce69dfb8f941d17d5e5fd892f43
Iran <ma1kelvdh@gmail.com>
2014-06-23 23:48:37 +0200

    Game no longer hard-codes at least 1 damage for infantry moving over TiberiumTypes when the type is set to have Power=0, so its now possible to have TiberiumTypes which dont damage infantry

--- src/tiberium_stuff.asm
+++ src/tiberium_stuff.asm
@@ -1,4 +1,5 @@
-; power=0 hard-coded to damage 1 at 0x004D3F84
+@CLEAR 0x004D3F8A 0x00 0x004D3F8B ; power=0 hard-coded to damage 1 at 0x004D3F84, set it to 00 instead
+; hard-coded power=17 for Vinifera at 0x00644DCA
 
 @JMP 0x0045ED08 _Tiberium_Chain_Reaction_Dont_Crash_With_Power_Zero
 
